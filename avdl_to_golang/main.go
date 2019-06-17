01d1fd75d5999194f6eac9badc35f05259cc6267
Andrew Arrow <andrew@bird.co>
2019-06-16 23:34:22 -0700

    start

---
+++ avdl_to_golang/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	/*for _, fileName := range cfg.files {
+		schema, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading file %q - %v\n", fileName, err)
+			os.Exit(2)
+		}
+
+		_, err = namespace.TypeForSchema(schema)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error decoding schema for file %q - %v\n", fileName, err)
+			os.Exit(3)
+		}
+	}*/
+}
