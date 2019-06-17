816d266e0832bdce11d948af05e3662e2f3bd6c9
Andrew Arrow <andrew@bird.co>
2019-06-17 07:52:54 +0000

    schema working

--- generator/process.go
+++ generator/process.go
@@ -5,6 +5,10 @@ import "strings"
 
 func ProcessLines(lines []string) {
 	recordOn := false
+	recordName := ""
+	f_strings := []string{}
+	f_floats := []string{}
+	f_longs := []string{}
 	for _, line := range lines {
 		line = strings.TrimSpace(line)
 		if strings.HasSuffix(line, "}") {
@@ -14,10 +18,31 @@ func ProcessLines(lines []string) {
 			tokens := strings.Split(line, " ")
 			dataType := tokens[0]
 			name := tokens[1][0 : len(tokens[1])-1]
-			fmt.Println(name, dataType)
+			if dataType == "string" {
+				f_strings = append(f_strings, name)
+			} else if dataType == "float" {
+				f_floats = append(f_floats, name)
+			} else if dataType == "long" {
+				f_longs = append(f_longs, name)
+			}
 		}
 		if strings.HasPrefix(line, "record") && strings.HasSuffix(line, "{") {
+			tokens := strings.Split(line, " ")
+			recordName = tokens[1]
 			recordOn = true
 		}
 	}
+
+	theFields := `"strings": "%s", "floats": "%s", "longs": "%s"`
+	filledIn := fmt.Sprintf(theFields, strings.Join(f_strings, ","),
+		strings.Join(f_floats, ","), strings.Join(f_longs, ","))
+
+	content := fmt.Sprintf("\"%s\": newFields(map[string]string{%s}),",
+		recordName, filledIn)
+
+	template := `var schema map[string]fields = map[string]fields{
+  %s
+}`
+
+	fmt.Printf(template, content)
 }
