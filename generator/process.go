1003f51c61942d43c8b6acfa7a8ba8af12ab992c
Andrew Arrow <andrew@bird.co>
2019-06-17 07:15:18 +0000

    on and off logic

--- generator/process.go
+++ generator/process.go
@@ -7,14 +7,17 @@ func ProcessLines(lines []string) {
 	recordOn := false
 	for _, line := range lines {
 		line = strings.TrimSpace(line)
-		if strings.HasPrefix(line, "record") && strings.HasSuffix(line, "{") {
-			recordOn = true
-		}
 		if strings.HasSuffix(line, "}") {
 			recordOn = false
 		}
 		if recordOn {
-			fmt.Println(line)
+			tokens := strings.Split(line, " ")
+			dataType := tokens[0]
+			name := tokens[1][0 : len(tokens[1])-1]
+			fmt.Println(name, dataType)
+		}
+		if strings.HasPrefix(line, "record") && strings.HasSuffix(line, "{") {
+			recordOn = true
 		}
 	}
 }
